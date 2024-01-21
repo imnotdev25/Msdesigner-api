dbea1cf200e83b3c60bbec242e5949061ffe3987
imnotdev25 <admin@opurl.co>
2024-01-21 22:06:37 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="MicrosoftDesigner",
+    version="0.0.1",
+    license="GNU General Public License v2.0",
+    author="Talaviya Bhavik",
+    author_email="talaviyabhavik@proton.me",
+    description="High quality image generation by Microsoft. Reverse engineered API.",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/imnotdev25/Msdesigner-api",
+    project_urls={
+        "Bug Report": "https://github.com/imnotdev25/Msdesigner-api/issues/new",
+    },
+    install_requires=[
+        "httpx",
+        "undetected-chromedriver",
+        "browser_cookie3",
+        "selenium"
+    ],
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    py_modules=["Msdesigner"],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
